b8dd17a701dc420060b521656b123eb628c8549d
Jakub Sokołowski <jakub@status.im>
2018-08-11 10:09:51 -0400

    minify html, add steps for fonts, upgrade packages

--- gulpfile.js
+++ gulpfile.js
@@ -13,31 +13,40 @@ var sourcemaps   = require('gulp-sourcemaps')
 
 var del          = require('del')
 var gutil        = require('gulp-util')
+var rename       = require('gulp-rename')
 var uglify       = require('gulp-uglify')
+var htmlmin      = require('gulp-htmlmin')
 
 var imagemin     = require('gulp-imagemin')
 
 gulp.task('js', function () {
-  return gulp.src('src/**/*.js', {read: false})
+  return gulp.src('src/js/**/*.js', {read: false})
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(tap(function (file) {
-      gutil.log('bundling ' + file.path)
+      //gutil.log('bundling ' + file.path)
       file.contents = browserify(file.path, {debug: true}).bundle()
     }))
     .pipe(streamify(babel({ presets: ['es2015', 'es2017'] })))
     .pipe(buffer())
     .pipe(uglify())
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist/js'))
     .pipe(browserSync.stream())
+})
+
+gulp.task('fonts', function() {
+  return gulp.src(['src/fonts**/*'])
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('html', function() {
   return gulp.src(['src/**/*.html'])
+    .pipe(htmlmin({collapseWhitespace: true, minifyJS: true}))
+    .pipe(rename(p => p.dirname = p.dirname.slice(5)))
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('sass', function() {
+gulp.task('css', function() {
   return gulp.src("src/scss/main.scss")
     .pipe(sass())
     .on('error', gutil.log)
@@ -46,7 +55,7 @@ gulp.task('sass', function() {
     .pipe(browserSync.stream())
 })
 
-gulp.task('imagemin', function() {
+gulp.task('images', function() {
   gulp.src('src/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'))
@@ -57,13 +66,13 @@ gulp.task('clean', function() {
 })
 
 gulp.task('develop', ['build'], function() {
-    browserSync.init({server: "./"})
+    browserSync.init({server: "./dist"})
 
-    gulp.watch("src/scss/*.scss", ['sass'])
+    gulp.watch("src/scss/*.scss", ['css'])
     gulp.watch("src/js/*.js", ['js'])
-    gulp.watch("src/img/**/*", ['imagemin'])
-    gulp.watch("./*.html").on('change', browserSync.reload)
+    gulp.watch("src/img/**/*", ['images'])
+    gulp.watch("./*.html", ['html']).on('change', browserSync.reload)
 })
 
-gulp.task('build',   ['clean', 'sass', 'imagemin', 'js'])
+gulp.task('build',   ['clean', 'css', 'images', 'fonts', 'js', 'html'])
 gulp.task('default', ['develop'])
