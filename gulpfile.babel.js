16cce1f9cc5445a365d135ce3e803766fcd574af
Jakub <i+github@always.fail>
2018-08-15 13:06:48 -0400

    Fix/builds (#14)
    * upgrade packages
    
    * remove dist files
    
    * remove old .babelrc config
    
    * ginore contents of dist folder
    
    * fix gulp build
    
    * fix name of project
    
    * this page is not hostted on github pages
    
    * move all files to src, remove duplicates
    
    * add an html gulp task for copying html
    
    * move html files to src/html
    
    * minify html, add steps for fonts, upgrade packages
    
    * use modern function formatting
    
    * add some basic scripts to package.json
    
    * use babel-preset-env
    
    * use babel-preset-env features like import in gulpfile
    
    * downgrade to gulp@3.9.0
    
    * remove useless pdf files
    
    * more npm updates
    
    * upgrade core dependencies
    
    * remove unused files

---
+++ gulpfile.babel.js
@@ -0,0 +1,76 @@
+import gulp from 'gulp'
+import browserSync from 'browser-sync'
+import sass from 'gulp-sass'
+import autoprefixer from 'gulp-autoprefixer'
+
+import browserify from 'browserify'
+import source from 'vinyl-source-stream'
+import streamify from 'gulp-streamify'
+import babel from 'gulp-babel'
+import tap from 'gulp-tap'
+import buffer from 'gulp-buffer'
+import sourcemaps from 'gulp-sourcemaps'
+
+import del from 'del'
+import gutil from 'gulp-util'
+import rename from 'gulp-rename'
+import uglify from 'gulp-uglify'
+import htmlmin from 'gulp-htmlmin'
+import imagemin from 'gulp-imagemin'
+
+gulp.task('js', () => {
+  return gulp.src('src/js/**/*.js', {read: false})
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(tap(f => {
+      f.contents = browserify(f.path, {debug: true}).bundle()
+    }))
+    .pipe(streamify(babel({presets: ['env']})))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('fonts', () => {
+  return gulp.src(['src/fonts**/*'])
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('html', () => {
+  return gulp.src(['src/**/*.html'])
+    .pipe(htmlmin({collapseWhitespace: true, minifyJS: true}))
+    .pipe(rename(p => p.dirname = p.dirname.slice(5)))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('css', () => {
+  return gulp.src("src/scss/main.scss")
+    .pipe(sass())
+    .on('error', gutil.log)
+    .pipe(autoprefixer({ browsers: ['last 3 versions'], cascade: false }))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('images', () => {
+  return gulp.src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+})
+
+gulp.task('clean', () => {
+  del.sync('dist/*')
+})
+
+gulp.task('develop', ['build'], () => {
+    browserSync.init({server: "./dist"})
+
+    gulp.watch("src/scss/*.scss", ['css'])
+    gulp.watch("src/js/*.js", ['js'])
+    gulp.watch("src/img/**/*", ['images'])
+    gulp.watch("./*.html", ['html']).on('change', browserSync.reload)
+})
+
+gulp.task('build',   ['clean', 'css', 'images', 'fonts', 'js', 'html'])
+gulp.task('default', ['develop'])
--- post-receive.php
+++
@@ -1,9 +0,0 @@
-<?php
-ignore_user_abort(true);
-set_time_limit(0);
-
-// TODO check against ip or secret hash
-if ( $_SERVER['HTTP_X_GITHUB_EVENT'] == 'push' ) {
-  shell_exec("sudo /var/www/status-site/deploy.sh");
-  echo file_get_contents("/var/www/status-site/build.log");
-}
